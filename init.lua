b1f00758abb147a0ffbd649129713ae24eae8204
GeorgOstrovski <ostrovski@google.com>
2015-08-06 12:33:13 +0100

    Return pprint
    ... to allow `local pprint = require 'pprint'`
    
    Also corrected indentation and unified style.

--- init.lua
+++ init.lua
@@ -10,21 +10,21 @@ require 'torch'
 
 -- Wraps quoted strings in apostraphes
 local function smart_quote(str)
-  if string.match( string.gsub(str,"[^'\"]",""), '^"+$' ) then
+  if string.match( string.gsub(str, "[^'\"]", ""), '^"+$' ) then
     return "'" .. str .. "'"
   end
-  return string.format("%q", str )
+  return string.format("%q", str)
 end
 
 
 local control_chars_translation = {
-  ["\a"] = "\\a",  ["\b"] = "\\b", ["\f"] = "\\f",  ["\n"] = "\\n",
-  ["\r"] = "\\r",  ["\t"] = "\\t", ["\v"] = "\\v",  ["\\"] = "\\\\"
+    ["\a"] = "\\a",  ["\b"] = "\\b", ["\f"] = "\\f",  ["\n"] = "\\n",
+    ["\r"] = "\\r",  ["\t"] = "\\t", ["\v"] = "\\v",  ["\\"] = "\\\\"
 }
 
 
 local function unescape_char(c)
-    return control_chars_translation[c]
+  return control_chars_translation[c]
 end
 
 
@@ -50,8 +50,8 @@ end
 
 
 local sort_orders_by_type = {
-  ['number'] = 1, ['boolean']  = 2, ['string'] = 3, ['table'] = 4,
-  ['tensor'] = 5, ['function'] = 6, ['userdata'] = 7, ['thread'] = 8
+    ['number'] = 1, ['boolean']  = 2, ['string'] = 3, ['table'] = 4,
+    ['tensor'] = 5, ['function'] = 6, ['userdata'] = 7, ['thread'] = 8
 }
 
 
@@ -66,8 +66,8 @@ end
 local function get_dictionary_keys(t)
   local length = #t
   local keys = {}
-  for k,_ in pairs(t) do
-    if is_dictionary_key(k, length) then table.insert(keys,k) end
+  for k, _ in pairs(t) do
+    if is_dictionary_key(k, length) then table.insert(keys, k) end
   end
   table.sort(keys, sort_keys)
   return keys
@@ -89,27 +89,27 @@ local Printer = {}
 
 function Printer:init(depth, inline)
   local pprintor = {
-    buffer = {},
-    depth = depth or 0,
-    inline = inline or false,
-    level = 0,
-    counters = {
-      ['function'] = 0,
-      ['userdata'] = 0,
-      ['thread'] = 0,
-      ['table'] = 0
-    },
-    pools = {
-      ['function'] = setmetatable({}, {__mode = "kv"}),
-      ['userdata'] = setmetatable({}, {__mode = "kv"}),
-      ['thread'] = setmetatable({}, {__mode = "kv"}),
-      ['table'] = setmetatable({}, {__mode = "kv"})
-    }
+      buffer = {},
+      depth = depth or 0,
+      inline = inline or false,
+      level = 0,
+      counters = {
+          ['function'] = 0,
+          ['userdata'] = 0,
+          ['thread'] = 0,
+          ['table'] = 0
+      },
+      pools = {
+          ['function'] = setmetatable({}, {__mode = "kv"}),
+          ['userdata'] = setmetatable({}, {__mode = "kv"}),
+          ['thread'] = setmetatable({}, {__mode = "kv"}),
+          ['table'] = setmetatable({}, {__mode = "kv"})
+      }
   }
 
   setmetatable(pprintor, {
-    __index = Printer,
-    __tostring = function(instance) return table.concat(instance.buffer) end
+      __index = Printer,
+      __tostring = function(instance) return table.concat(instance.buffer) end
   } )
   return pprintor
 end
@@ -123,7 +123,7 @@ end
 
 function Printer:puts(...)
   local args = {...}
-  for i=1, #args do
+  for i = 1, #args do
     table.insert(self.buffer, tostring(args[i]))
   end
   return self
@@ -164,33 +164,33 @@ function Printer:put_table(t)
   else
     self:down()
 
-      local length = #t
-      local mt = getmetatable(t)
-      self:puts('{')
+    local length = #t
+    local mt = getmetatable(t)
+    self:puts('{')
 
-      local string = get_to_string_result_safely(t, mt)
-      if type(string) == 'string' and #string > 0 then
-        self:puts(' -- ', unescape(string))
-        if length >= 1 then self:tabify() end -- tabify the array values
-      end
+    local string = get_to_string_result_safely(t, mt)
+    if type(string) == 'string' and #string > 0 then
+      self:puts(' -- ', unescape(string))
+      if length >= 1 then self:tabify() end -- tabify the array values
+    end
 
-      local comma = false
-      for i=1, length do
-        comma = self:put_comma(comma)
-        self:puts(' '):put_value(t[i])
-      end
+    local comma = false
+    for i = 1, length do
+      comma = self:put_comma(comma)
+      self:puts(' '):put_value(t[i])
+    end
 
-      local dict_keys = get_dictionary_keys(t)
+    local dict_keys = get_dictionary_keys(t)
 
-      for _,k in ipairs(dict_keys) do
-        comma = self:put_comma(comma)
-        self:tabify():put_key(k):puts(' = '):put_value(t[k])
-      end
+    for _, k in ipairs(dict_keys) do
+      comma = self:put_comma(comma)
+      self:tabify():put_key(k):puts(' = '):put_value(t[k])
+    end
 
-      if mt then
-        comma = self:put_comma(comma)
-        self:tabify():puts('<metatable> = '):put_value(mt)
-      end
+    if mt then
+      comma = self:put_comma(comma)
+      self:tabify():puts('<metatable> = '):put_value(mt)
+    end
     self:up()
 
     if #dict_keys > 0 or mt then -- dictionary table. Justify closing }
@@ -205,28 +205,28 @@ end
 
 
 function Printer:put_tensor(t)
-    local size = t:nElement()
-    if t:nElement() <= 20 then
-        self:puts(tostring(t))
-    else
-        self:puts('[' .. torch.typename(t) .. ' of dimension ')
-        self:put_tensor_dims(t)
-        self:puts(']')
-    end
+  local size = t:nElement()
+  if t:nElement() <= 20 then
+    self:puts(tostring(t))
+  else
+    self:puts('[' .. torch.typename(t) .. ' of dimension ')
+    self:put_tensor_dims(t)
+    self:puts(']')
+  end
 end
 
 
 function Printer:put_tensor_dims(t)
-   local n_dims = t:dim()
-   local dim_sizes = t:size()
-   if n_dims == 0 then
-      self:puts('[0-dimensional tensor]')
-   else
-      for i=1, n_dims-1 do
-        self:puts(dim_sizes[i]):puts('x')
-      end
-      self:puts(dim_sizes[n_dims])
-   end
+  local n_dims = t:dim()
+  local dim_sizes = t:size()
+  if n_dims == 0 then
+    self:puts('[0-dimensional tensor]')
+  else
+    for i = 1, n_dims-1 do
+      self:puts(dim_sizes[i]):puts('x')
+    end
+    self:puts(dim_sizes[n_dims])
+  end
 end
 
 
@@ -281,35 +281,35 @@ end
 
 
 local function print_tensor_dimensions(t)
-   local p = Printer.init()
-   p:put_tensor_dims(t)
-   return tostring(p)
+  local p = Printer.init()
+  p:put_tensor_dims(t)
+  return tostring(p)
 end
 
 
 local function print_tensor_info(t)
-   local p = Printer.init()
-
-   -- if we find a table, recursively call info on its elements
-   if type(t) == 'table' then
-      local newline = ' '
-      if #t > 1 then newline = '\n' end
-      p:puts('{'..newline)
-      for i=1,#t do
-         p:puts(print_tensor_info(t[i]))
-         p:puts(newline)
-      end
-      p:puts('}')
-   else
-      if t == nil then p:puts("[nil]") end
-      p:puts('['):put_tensor_dims(t)
-      if t.min then p:puts(', min: '):put_value(t:min()) end
-      if t.mean then p:puts(', mean: '):put_value(t:mean()) end
-      if t.max then p:puts(', max: '):put_value(t:max()) end
-      if t.type then p:puts(', type: '):put_value(t:type()) end
-      p:puts(']')
-   end
-   return tostring(p)
+  local p = Printer.init()
+  
+  -- if we find a table, recursively call info on its elements
+  if type(t) == 'table' then
+    local newline = ' '
+    if #t > 1 then newline = '\n' end
+    p:puts('{' .. newline)
+    for i = 1, #t do
+      p:puts(print_tensor_info(t[i]))
+      p:puts(newline)
+    end
+    p:puts('}')
+  else
+    if t == nil then p:puts("[nil]") end
+    p:puts('['):put_tensor_dims(t)
+    if t.min then p:puts(', min: '):put_value(t:min()) end
+    if t.mean then p:puts(', mean: '):put_value(t:mean()) end
+    if t.max then p:puts(', max: '):put_value(t:max()) end
+    if t.type then p:puts(', type: '):put_value(t:type()) end
+    p:puts(']')
+  end
+  return tostring(p)
 end
 
 
@@ -325,23 +325,24 @@ local function string(t, depth)
 end
 
 local function pprint_pprint(self, data, depth)
-    print(pretty_string(data, depth))
+  print(pretty_string(data, depth))
 end
 
 pprint = {
-    pretty_string=pretty_string, -- multi-line string
-    string=string, -- inline string
-    dims=print_tensor_dimensions,
-    info=print_tensor_info,
-    __call=pprint_pprint,
-    printer = function(depth) depth = depth or 4
+    pretty_string = pretty_string, -- multi-line string
+    string = string, -- inline string
+    dims = print_tensor_dimensions,
+    info = print_tensor_info,
+    __call = pprint_pprint,
+    printer = function(depth)
+        depth = depth or 4
         local function pretty_string(...)
             local str_table = {}
-            for i = 1,select('#',...) do
-                local obj = select(i,...)
-               table.insert(str_table, tostring(Printer:new(obj, depth)))
+            for i = 1, select('#', ...) do
+              local obj = select(i, ...)
+              table.insert(str_table, tostring(Printer:new(obj, depth)))
             end
-            return table.concat(str_table,' ')
+            return table.concat(str_table, ' ')
         end
         return pretty_string
     end
@@ -351,3 +352,4 @@ pprint = {
 setmetatable(pprint, pprint)
 
 
+return pprint
